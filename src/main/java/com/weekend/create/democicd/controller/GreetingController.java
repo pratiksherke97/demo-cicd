92325fd9efeeca108d4e005c838f0bbcc710751c
pratik <pratiksherke97@gmail.com>
2020-06-01 17:36:32 +0530

    Adding base path message

--- src/main/java/com/weekend/create/democicd/controller/GreetingController.java
+++ src/main/java/com/weekend/create/democicd/controller/GreetingController.java
@@ -2,6 +2,7 @@ package com.weekend.create.democicd.controller;
 
 import com.weekend.create.democicd.dto.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class GreetingController {
   private static final String TEMPLATE = "Hello, %s!";
 
   private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/")
+  String get() {
+    return "This is a Basic Application deployed to Heroku Container Registry";
+  }
 
   @GetMapping("/greeting")
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "Java") String name) {
